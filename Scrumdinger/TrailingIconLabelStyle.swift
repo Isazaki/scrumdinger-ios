84a27857c85786ea28c3711338ee9d1f662ccd29
Kondwani <kondwanimsiska@gmail.com>
2022-12-28 21:07:13 +0200

    chore(app): views and theming

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,23 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Kondwani Msiska on 27/12/2022.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+
+    static var trailingIcon: Self { Self() }
+
+}
