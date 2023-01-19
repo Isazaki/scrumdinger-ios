66e3bfea4dc2bbe6ff0de2f4e619590a0ef98a73
Kondwani <kondwanimsiska@gmail.com>
2023-01-19 21:27:52 +0200

    chore(app): drawing the timer view

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -13,10 +13,23 @@ class ScrumStore: ObservableObject {
     
     private static func fileURL() throws -> URL {
         try FileManager.default.url(for: .documentDirectory,
-                                    in: .userDomainMask,
-                                    appropriateFor: nil,
-                                    create: false)
-        .appendingPathComponent("scrums.data")
+                                       in: .userDomainMask,
+                                       appropriateFor: nil,
+                                       create: false)
+            .appendingPathComponent("scrums.data")
+    }
+    
+    static func load() async throws -> [DailyScrum] {
+        try await withCheckedThrowingContinuation { continuation in
+            load { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                case .success(let scrums):
+                    continuation.resume(returning: scrums)
+                }
+            }
+        }
     }
     
     static func load(completion: @escaping (Result<[DailyScrum], Error>)->Void) {
@@ -40,32 +53,35 @@ class ScrumStore: ObservableObject {
             }
         }
     }
+    
+    @discardableResult
+    static func save(scrums: [DailyScrum]) async throws -> Int {
+        try await withCheckedThrowingContinuation { continuation in
+            save(scrums: scrums) { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                case .success(let scrumsSaved):
+                    continuation.resume(returning: scrumsSaved)
+                }
+            }
+        }
+    }
+    
     static func save(scrums: [DailyScrum], completion: @escaping (Result<Int, Error>)->Void) {
-        
         DispatchQueue.global(qos: .background).async {
-            
             do {
                 let data = try JSONEncoder().encode(scrums)
-                
                 let outfile = try fileURL()
                 try data.write(to: outfile)
-                
                 DispatchQueue.main.async {
-                    
                     completion(.success(scrums.count))
-                    
                 }
-                
             } catch {
                 DispatchQueue.main.async {
-                    
                     completion(.failure(error))
-                    
                 }
-                
             }
-            
         }
-        
     }
 }
--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -11,16 +11,30 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     @StateObject private var store = ScrumStore()
+    @State private var errorWrapper: ErrorWrapper?
     
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ScrumsView(scrums: $store.scrums) { 
-                    ScrumStore.save(scrums: store.scrums) { result in
-                        if case .failure(let error) = result {
-                            fatalError(error.localizedDescription)
-                        }
-                        
+                ScrumsView(scrums: $store.scrums) {
+                    Task {
+                        do {
+                            try await ScrumStore.save(scrums: store.scrums)
+                        } catch {
+                            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")                        }
+                    }
+                }
+                .sheet(item: $errorWrapper, onDismiss: {
+                    store.scrums = DailyScrum.sampleData
+                    
+                }) { wrapper in
+                    ErrorView(errorWrapper: wrapper)
+                }
+                .task {
+                    do {
+                        store.scrums = try await ScrumStore.load()
+                    } catch {
+                        fatalError("Error loading scrums")
                     }
                 }
             }
